99b6ebe732ed57d65298b47ba36824d065bb6dd9
ryznoxy <137681244+ryznoxy@users.noreply.github.com>
2023-10-18 08:12:16 +0700

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,29 @@ hamburgerBtn.addEventListener("click", () => {
 //   }
 // });
 
-window.onload = function () {
-  document.body.classList.add("loaded");
-};
+// window.onload = function () {
+//   document.body.classList.add("loaded");
+// };
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`nav a[href*=${sectionId}]`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`nav a[href*=${sectionId}]`)
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
